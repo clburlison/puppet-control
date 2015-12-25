e5dc2bbb114fe39df395ec2bcdae8176ebcaa88a
Clayton Burlison <clburlison@gmail.com>
2015-12-25 01:32:24 -0600

    add python based fact
    the bash version wasn't working ¯\_(ツ)_/¯

--- site/profile/facts.d/boxen_home.sh
+++
@@ -1,3 +0,0 @@
-#/bin/bash
-CURRENT_USER=`/bin/ls -l /dev/console | /usr/bin/awk '{ print $3 }'`
-echo "boxen_home=/Users/${CURRENT_USER}/Dropbox/Config/boxen_home"
---
+++ site/profile/facts.d/boxen_user.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import os
+import subprocess
+
+def get_console_user():
+    '''Returns the currently logged-in user as
+    a string, even if running as EUID root.'''
+    if os.geteuid() == 0:
+        console_user = subprocess.check_output(['/usr/bin/stat',
+                                                '-f%Su',
+                                                '/dev/console']).strip()
+    else:
+        import getpass
+        console_user = getpass.getuser()
+    return console_user
+
+print "boxen_user="+get_console_user()
