76e79bfbdbe48495a940751a69a087fbeeade61a
Clayton Burlison <clburlison@gmail.com>
2015-12-25 01:48:37 -0600

    factsync update

--- site/profile/manifests/puppet/factsync.pp
+++ site/profile/manifests/puppet/factsync.pp
@@ -1,21 +1,22 @@
 # A hack to get around the fact that pluginsync doesn't work properly
-# when using 'puppet apply'
+# when using 'puppet apply'.
 # Will recursively sync the fact.d folder to the right directory.
-class profile::puppet::factsync (
-  $factdir2 = "/etc/facter/facts.d",
-  $factdir3 = "/opt/puppetlabs/facter/facts.d/",
-) {
-  # file {"$factdir2":
-  #   source  => "/private/etc/puppet/environments/production/modules/profile/facts.d",
-  #   recurse => true,
-  #   purge   => true,
-  #   force   => true,
-  # }
+class profile::puppet::factsync {
   
-  file {"$factpath3":
-    source  => "/private/etc/puppetlabs/code/environments/production/site/profile/facts.d",
-    recurse => true,
-    purge   => true,
-    force   => true,
-  }  
+  if $facterversion >= '3.0.0' {
+    file {"/opt/puppetlabs/facter/facts.d/":
+      source  => "/private/etc/puppetlabs/code/environments/production/site/profile/facts.d",
+      recurse => true,
+      purge   => true,
+      force   => true,
+    }
+  }
+  else {
+    file {"/etc/facter/facts.d":
+      source  => "/private/etc/puppet/environments/production/modules/profile/facts.d",
+      recurse => true,
+      purge   => true,
+      force   => true,
+    }
+  }
 }
