d016aa1793101520d1045430248120e4dadc23ae
Clayton Burlison <clburlison@gmail.com>
2016-01-04 00:56:59 -0600

    Use Puppet to install required gems

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,3 +1,43 @@
-node default {
+# Required gems that osx nodes must have to work with out masterless setup
+class osx_gems {
+  package { 'r10k':
+    ensure          => '2.1.1',
+    provider        => 'puppet_gem',
+    source          => 'https://production.cf.rubygems.org',
+    install_options => [ '--install-dir', '/opt/puppetlabs/puppet/lib/ruby/gems/2.1.0', '--bindir', '/opt/puppetlabs/puppet/bin' ],
+  }
+
+  package { 'hiera-eyaml':
+    ensure          => '2.0.8',
+    provider        => 'puppet_gem',
+    source          => 'https://production.cf.rubygems.org',
+    install_options => [ '--install-dir', '/opt/puppetlabs/puppet/lib/ruby/gems/2.1.0', '--bindir', '/opt/puppetlabs/puppet/bin' ],
+  }
+  
+  package { 'sqlite3':
+    ensure          => '1.3.11',
+    provider        => 'puppet_gem',
+    source          => 'https://production.cf.rubygems.org',
+    install_options => [ '--install-dir', '/opt/puppetlabs/puppet/lib/ruby/gems/2.1.0', '--bindir', '/opt/puppetlabs/puppet/bin' ],
+  }
+  
+  package { 'CFPropertyList':
+    ensure          => '2.3.2',
+    provider        => 'puppet_gem',
+    source          => 'https://production.cf.rubygems.org',
+    install_options => [ '--install-dir', '/opt/puppetlabs/puppet/lib/ruby/gems/2.1.0', '--bindir', '/opt/puppetlabs/puppet/bin' ],
+  }
+}
+
+# Class to allow ordering of gems then hiera classes
+class include {
   hiera_include('classes')
+}
+
+# Define default node
+node default {
+  if $::osfamily == 'Darwin' {
+      Class {'osx_gems':} ->
+      Class {'include':}
+  }
 }
