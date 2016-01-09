9fb4066e1156d4d360a35fe79243622f696f93af
Clayton Burlison <clburlison@gmail.com>
2016-01-08 18:29:00 -0600

    new hiera data local for puppet4

--- site/profile/manifests/puppet/puppet4_hiera.pp
+++ site/profile/manifests/puppet/puppet4_hiera.pp
@@ -1,7 +1,7 @@
 # Make sure /etc/puppetlabs/code/hiera.yaml is always
 # the most up to date version.
 class profile::puppet::puppet4_hiera {
-  file { "${::codedir}/hiera.yaml":
-    source  => "${::codedir}/${::environment}/hieradata/hiera.yaml",
+  file { "/etc/puppetlabs/code/hiera.yaml":
+    source  => "/etc/puppetlabs/code/environments/production/hieradata/hiera.yaml",
   }
 }
