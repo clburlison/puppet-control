76e7f27059aa8de8629c01933cc1fd4f001d9ad2
Clayton Burlison <clburlison@gmail.com>
2016-02-08 18:46:16 -0600

    few updates.

--- site/profile/facts.d/console_user.py
+++ site/profile/facts.d/console_user.py
@@ -2,19 +2,23 @@
 """Get current console user and print facter 
 fact to standard out"""
 
-import os
-import subprocess
+# import os
+# import subprocess
+#
+# def get_console_user():
+#     """Returns the currently logged-in user as
+#     a string, even if running as EUID root."""
+#     if os.geteuid() == 0:
+#         console_user = subprocess.check_output(['/usr/bin/stat',
+#                                                 '-f%Su',
+#                                                 '/dev/console']).strip()
+#     else:
+#         import getpass
+#         console_user = getpass.getuser()
+#     return console_user
 
-def get_console_user():
-    """Returns the currently logged-in user as
-    a string, even if running as EUID root."""
-    if os.geteuid() == 0:
-        console_user = subprocess.check_output(['/usr/bin/stat',
-                                                '-f%Su',
-                                                '/dev/console']).strip()
-    else:
-        import getpass
-        console_user = getpass.getuser()
-    return console_user
+from SystemConfiguration import SCDynamicStoreCopyConsoleUser
+username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]
+username = [username,""][username in [u"loginwindow", None, u""]]
 
-print "console_user="+get_console_user()
+print "console_user="+username
--- site/profile/facts.d/dropbox_presence.py
+++ site/profile/facts.d/dropbox_presence.py
@@ -3,24 +3,28 @@
 Dropbox directory."""
 
 import os
-import subprocess
+# import subprocess
+#
+# def get_console_user():
+#     """Returns the currently logged-in user as
+#     a string, even if running as EUID root."""
+#     if os.geteuid() == 0:
+#         console_user = subprocess.check_output(['/usr/bin/stat',
+#                                                 '-f%Su',
+#                                                 '/dev/console']).strip()
+#     else:
+#         import getpass
+#         console_user = getpass.getuser()
+#     return console_user
 
-def get_console_user():
-    """Returns the currently logged-in user as
-    a string, even if running as EUID root."""
-    if os.geteuid() == 0:
-        console_user = subprocess.check_output(['/usr/bin/stat',
-                                                '-f%Su',
-                                                '/dev/console']).strip()
-    else:
-        import getpass
-        console_user = getpass.getuser()
-    return console_user
+from SystemConfiguration import SCDynamicStoreCopyConsoleUser
+username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]
+username = [username,""][username in [u"loginwindow", None, u""]]
 
 def dropbox_presence():
     """Checks for the current console users Dropbox folder. 
     If it is present return True else False"""
-    dir = "/Users/" + get_console_user() + "/Dropbox"
+    dir = "/Users/" + username + "/Dropbox"
     if not os.path.exists(dir):
         return False
     else:
