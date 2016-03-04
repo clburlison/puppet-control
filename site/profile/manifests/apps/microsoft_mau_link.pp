3f3ba2cea6bd8609187f15edcf453a9a2ec6997f
Clayton Burlison <clburlison@gmail.com>
2016-03-04 09:41:43 -0600

    enable insider builds

--- site/profile/manifests/apps/microsoft_mau_link.pp
+++ site/profile/manifests/apps/microsoft_mau_link.pp
@@ -1,12 +1,21 @@
 class profile::apps::microsoft_mau_link (
 ){
-    File {
-        owner   => "root",
-        group   => 'admin',
-    }
 
-    file {"/Applications/Utilities/Microsoft AutoUpdate.app":
-        ensure => link,
-        target => "/Library/Application Support/Microsoft/MAU2.0/Microsoft AutoUpdate.app",
-    }
+  # Enable insider builds from the Internal Channel
+  mac_admin::osx_defaults { 'Enable MAU Internal Builds':
+      ensure => present,
+      domain => 'com.microsoft.autoupdate2',
+      key    => 'ChannelName',
+      value  => 'Internal',
+  }
+
+  File {
+      owner   => "root",
+      group   => 'admin',
+  }
+
+  file {"/Applications/Utilities/Microsoft AutoUpdate.app":
+      ensure => link,
+      target => "/Library/Application Support/Microsoft/MAU2.0/Microsoft AutoUpdate.app",
+  }
 }
