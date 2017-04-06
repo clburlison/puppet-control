fbfb5bb0893e8632f4913039f51b28bcb5c72abc
Clayton Burlison <github@clburlison.com>
2017-04-06 15:48:50 -0500

    Don't link as many dirs any longer. HOME was getting messy

--- site/profile/manifests/directories.pp
+++ site/profile/manifests/directories.pp
@@ -5,7 +5,7 @@ class profile::directories (
     $my_username  = $profile::my_username
 
     require profile::puppet::factsync
-    
+
     File {
         owner   => "${my_username}",
         group   => 'admin',
@@ -21,33 +21,33 @@ class profile::directories (
         target => "/Users/${my_username}/Dropbox/Config/User/bin",
     }
 
-    file {"${my_homedir}/blog":
-        ensure => link,
-        target => "/Users/${my_username}/Dropbox/src/mine/blog",
-    }
-
-    file {"${my_homedir}/.dotfiles":
-        ensure => link,
-        target => "/Users/${my_username}/Dropbox/src/mine/dotfiles",
-    }
-
-    file {"${my_homedir}/mine":
-        ensure => link,
-        target => "/Users/${my_username}/Dropbox/src/mine",
-    }
-
-    file {"${my_homedir}/others":
-        ensure => link,
-        target => "/Users/${my_username}/Dropbox/src/others",
-    }
-
-    file {"${my_homedir}/puppet":
-        ensure => link,
-        target => "/Users/${my_username}/Dropbox/src/puppet",
-    }
-
-    file {"${my_homedir}/work":
-        ensure => link,
-        target => "/Users/${my_username}/Dropbox/src/work",
-    }
+#     file {"${my_homedir}/blog":
+#         ensure => link,
+#         target => "/Users/${my_username}/Dropbox/src/mine/blog",
+#     }
+#
+#     file {"${my_homedir}/.dotfiles":
+#         ensure => link,
+#         target => "/Users/${my_username}/Dropbox/src/mine/dotfiles",
+#     }
+#
+#     file {"${my_homedir}/mine":
+#         ensure => link,
+#         target => "/Users/${my_username}/Dropbox/src/mine",
+#     }
+#
+#     file {"${my_homedir}/others":
+#         ensure => link,
+#         target => "/Users/${my_username}/Dropbox/src/others",
+#     }
+#
+#     file {"${my_homedir}/puppet":
+#         ensure => link,
+#         target => "/Users/${my_username}/Dropbox/src/puppet",
+#     }
+#
+#     file {"${my_homedir}/work":
+#         ensure => link,
+#         target => "/Users/${my_username}/Dropbox/src/work",
+#     }
 }
